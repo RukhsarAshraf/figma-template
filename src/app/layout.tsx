f7597f07705ade835855e3ff903aa0b9521f4d6a
Rukhsar Ashraf <rukhsarashraf@gmail.com>
2025-02-07 13:02:26 +0500

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Providers from "./components/providers";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
